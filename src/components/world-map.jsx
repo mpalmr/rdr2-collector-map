d1816f2f59cf298f7704d7e8ab1f5e1d06cd60e6
Matthew Palmer <matt@silvercreekit.com>
2019-10-25 08:11:13 -0400

    zoom controls

--- src/components/world-map.jsx
+++ src/components/world-map.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, ZoomControl } from 'react-leaflet';
 
 export default function WorldMap() {
 	return (
@@ -8,14 +8,15 @@ export default function WorldMap() {
 			zoom={4}
 			maxBounds={[[114, 80], [-10, -176]]}
 			easeLinearity={0.35}
+			zoomControl={false}
 			attributionControl
-			zoomControl
 			doubleClickZoom
 			scrollWheelZoom
 			dragging
 			animate
 		>
 			<TileLayer url="/tiles/detailed/{z}/{x}_{y}.jpg" />
+			<ZoomControl position="topright" />
 		</Map>
 	);
 }
