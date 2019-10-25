a6197c6283f1b544012b1f92e859a8ac29ee4629
Matthew Palmer <matt@silvercreekit.com>
2019-10-25 05:10:40 -0400

    add detailed layer images

--- src/create-map.js
+++ src/create-map.js
@@ -8,18 +8,19 @@ export default function createMap() {
 			maxZoom: 7,
 			zoomControl: false,
 			crs: L.CRS.Simple,
-			layers: [L.tileLayer('https://s.rsg.sc/sc/images/games/RDR2/map/game/{z}/{x}/{y}.jpg', {
+			layers: [L.tileLayer('/tiles/detailed/{z}/{x}_{y}.jpg', {
 				noWrap: true,
 				bounds: L.latLngBounds(L.latLng(-144, 0), L.latLng(0, 176)),
 			})],
 		});
 		// .setView([-70, 111.75]);
+	L.control
+		.zoom({ position: 'bottomright' })
+		.addTo(map);
 	map.setMaxBounds(L.latLngBounds(
 		L.latLng(-170.712, -25.227),
 		L.latLng(10.774, 200.125),
 	));
-	L.control
-		.zoom({ position: 'bottomright' })
-		.addTo(map);
+	L.control.layers().addTo(map);
 	return map;
 }
